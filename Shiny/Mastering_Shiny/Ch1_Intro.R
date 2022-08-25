f753cffdd3e41e1c700bad08ec0485cea14af21c
Matt Smith <mdsmith44@gmail.com>
2022-08-24 21:27:54 -0700

    wtf

--- Shiny/Mastering_Shiny/Ch1_Intro.R
+++ Shiny/Mastering_Shiny/Ch1_Intro.R
@@ -1,10 +1,19 @@
 library(shiny)
 
+#Basic boilerplate is a ui to describe how app looks,
+#and a server to describe how it works, plus a call
+#to shinyApp(ui,server)
+# App is typically contained in a file app.R, and in fact
+#when running a shiny app from a folder that folder must
+#have a file named app.R.
+#more
+
 ui <- fluidPage(
   selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
   verbatimTextOutput("summary"),
   tableOutput("table")
 )
+
 server <- function(input, output, session) {
 }
 shinyApp(ui, server)
